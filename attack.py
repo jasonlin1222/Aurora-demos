a79f77d92e444ecca6e5049bf01bc6d33abfa1b6
jasonlin1222 <jasonlin1222@gmail.com>
2026-01-21 00:32:25 -0600

    change msf config path

--- attack.py
+++ attack.py
@@ -28,7 +28,7 @@ def confirm_action(prompt: str = "Keep going with the next attack step?") -> boo
 async def main():
     print_welcome_message()
     from attack_executor.config import load_config
-    config = load_config(config_file_path="/home/kali/Desktop/Aurora-executor-demo/config.ini")
+    config = load_config(config_file_path="/home/kali/Desktop/Aurora-demos/docs/config.ini")
     from attack_executor.exploit.Metasploit import MetasploitExecutor
     metasploit_executor = MetasploitExecutor(config=config)
 
